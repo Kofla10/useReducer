80ed7ff3598fe4adfc1230aa1d8fd86df9e0312e
kofla10 <juank_milo94@hotmail.com>
2024-09-17 21:10:03 -0500

    start use reducer

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import './useReducer/intro-reducer.js'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    
+  </StrictMode>,
+)
---
+++ src/useReducer/intro-reducer.js
@@ -0,0 +1,11 @@
+const initialState =[{
+  id: 1,
+  todo: 'Recolectar la piedra del alma',
+  done: false,
+}]
+
+const todoReducer = ( state = initialState, action ) => {
+
+  return state;
+
+}
